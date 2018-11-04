c0419aaca5b929b1b000ece1f501ab8c2b6de175
Kenneth Sim <kensim28@hotmail.com>
2018-11-05 04:04:06 +0800

    4

---
+++ InfoSys1D/app/src/main/java/com/example/kensi/infosys1d/LoginPostRequest.java
@@ -0,0 +1,106 @@
+package com.example.kensi.infosys1d;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginPostRequest {
+    public static String main(String password, String email, boolean remember) {
+
+        if (!emailCheck(email)){
+            return "E-mail wrong format";
+        }
+
+        if (password.length()<6){
+            return "Password too short";
+        }
+        if (password.length()>128){
+            return "Password too long";
+        }
+
+        try {
+            String rememberString;
+            if (remember){
+                rememberString="1";
+            }
+            else{
+                rememberString="0";
+            }
+            return call_me(password, email, rememberString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("MYAPP", "exception", e);
+            return "check log";
+        }
+    }
+
+    public static String call_me(String password, String email, String rememberString) throws Exception {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("password", password);
+        parameters.put("email", email);
+        parameters.put("remember", rememberString);
+        String data = ParameterStringBuilder.getParamsString(parameters);
+        String url = "https://chocolatepie.tech/admin/login";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes(data);
+        out.flush();
+        out.close();
+        int responseCode = con.getResponseCode();
+//        System.out.println("\nSending 'POST' request to URL : " + url);
+//        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+//        System.out.println(response.toString());
+        return response.toString();
+    }
+
+    public static boolean emailCheck (String email){
+        if (email.length()<7 || email.length()>128){
+            return false;
+        }
+        char[] emailChar = email.toCharArray();
+        int [] unwanted = {32,40, 41, 60, 62, 72, 73};
+        for (char c : emailChar){
+            int ascii = (int) c;
+            if (ascii < 0||ascii > 176||contains(unwanted, ascii)){
+                return false;
+            }
+        }
+        if (!email.contains(".")){
+            return false;
+        }
+        String[] atCheck = email.split("@");
+        if (atCheck.length!=2){
+            return false;
+        }
+        if (atCheck[0].length()<1 || atCheck[0].length()>64 || atCheck[1].length()<1){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean contains(int[] arr, int check){
+        for (int n : arr) {
+            if (n == check) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ InfoSys1D/app/src/main/java/com/example/kensi/infosys1d/test.java
@@ -0,0 +1,13 @@
+package com.example.kensi.infosys1d;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.simple.parser.JSONParser;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        String serverReply = LoginPostRequest.main("asdadasd","asdnsnioad@uiasuias.com",true);
+        System.out.println(serverReply.toCharArray()[11]);
+    }
+}
