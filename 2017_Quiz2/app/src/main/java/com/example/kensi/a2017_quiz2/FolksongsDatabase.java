a82013dbae37d5d7d438b5f07fb9eef16456c513
Kenneth Sim <kensim28@hotmail.com>
2018-12-12 19:47:21 +0800

    16

--- 2017_Quiz2/app/src/main/java/com/example/kensi/a2017_quiz2/FolksongsDatabase.java
+++ 2017_Quiz2/app/src/main/java/com/example/kensi/a2017_quiz2/FolksongsDatabase.java
@@ -6,17 +6,26 @@ import android.provider.BaseColumns;
 
 public class FolksongsDatabase {
 
+
+
     public static final class FolksongsTable implements BaseColumns{
 
         public static final String TABLE_NAME = "Folksongs";
         public static final String COL_TITLE = "title";
         public static final String COL_COUNTRY = "country";
 
+        public static String SPACE = " ";
+        public static String COMMA = ",";
+
         //TODO 1 - make the SQL commands to create a table and delete a table
-//        public static final String SQL_CREATE_TABLE = “”;
-//
-//        public static final String SQL_DELETE_TABLE = “”;
-//
+        public static final String SQL_CREATE_TABLE = "CREATE TABLE" + SPACE
+                + TABLE_NAME + SPACE + "("
+                + _ID + SPACE + "INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA
+                + COL_TITLE + SPACE + "TEXT NOT NULL" + COMMA
+                + COL_COUNTRY + SPACE + "TEXT NOT NULL" + ");" ;
+
+        public static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
 
     }
 }
--- 2017_Quiz2/app/src/main/java/com/example/kensi/a2017_quiz2/FolksongsDbHelper.java
+++ 2017_Quiz2/app/src/main/java/com/example/kensi/a2017_quiz2/FolksongsDbHelper.java
@@ -4,11 +4,24 @@ package com.example.kensi.a2017_quiz2;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static android.provider.ContactsContract.Directory.PACKAGE_NAME;
 
 
 public class FolksongsDbHelper extends SQLiteOpenHelper {
@@ -25,7 +38,8 @@ public class FolksongsDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(FolksongsDatabase.FolksongsTable.SQL_CREATE_TABLE);
+        fillTable(db);
     }
 
     @Override
@@ -36,4 +50,94 @@ public class FolksongsDbHelper extends SQLiteOpenHelper {
     //TODO 2.1 - override the necessary methods
     //TODO 2.2 - write the method that fills the table when it is created
 
+    private void fillTable(SQLiteDatabase sqLiteDatabase){
+
+        ArrayList<CharaData> arrayList = new ArrayList<>();
+        PACKAGE_NAME = context.getPackageName();
+
+        //open the Json file pictures stored in the res/raw folder
+        InputStream inputStream = context.getResources().openRawResource(R.raw.pictures);
+        String string = MainActivity.convertJsonToString(R.raw.folksongs);
+
+        //parse the Json file and store data in the ArrayList using the CharaData class
+        try{
+            JSONArray jsonArray = new JSONArray(string);
+            for(int i = 0; i <= jsonArray.length(); i++){
+                String name = jsonArray.getJSONObject(i).getString("title");
+                String description = jsonArray.getJSONObject(i).getString("country");
+
+                arrayList.add(new CharaData(name, description));
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        //Each entry in the arrayList is stored as a ContentValues object
+        //Then this ContentValues object is inserted to the sqLiteDatabase to create a new row
+        for(int i = 0; i< arrayList.size(); i++){
+            Log.i("Norman","" + arrayList.get(i).getCountry());
+            ContentValues cv = new ContentValues();
+
+            cv.put(CharaContract.CharaEntry.COL_NAME, arrayList.get(i).getTitle());
+            cv.put(CharaContract.CharaEntry.COL_DESCRIPTION, arrayList.get(i).getCountry());
+
+            String fname = arrayList.get(i).getFile();
+            int resId = context.getResources().getIdentifier(fname, "drawable", PACKAGE_NAME);
+            Drawable drawable = context.getResources().getDrawable(resId);
+            Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] bitMapData = stream.toByteArray();
+
+            cv.put(CharaContract.CharaEntry.COL_FILE,bitMapData);
+
+            sqLiteDatabase.insert(CharaContract.CharaEntry.TABLE_NAME,null,cv);
+        }
+
+        Cursor cursor = sqLiteDatabase.rawQuery(CharaContract.CharaSql.SQL_QUERY_ALL_ROWS, null);
+        Log.i("Norman","Table Filled. Rows = " + cursor.getCount());
+
+
+
+    }
+    private String convertJsonToString(int resource) {
+
+        String line;
+        String output = "";
+
+        InputStream inputStream = getResources().openRawResource(resource);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                output = output + line;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return output;
+    }
+
+
+
+    static class CharaData{
+
+        private String title;
+        private String country;
+
+        public CharaData(String title, String country) {
+            this.title = title;
+            this.country = country;
+        }
+
+
+        public String getCountry() {
+            return country;
+        }
+
+
+        public String getTitle() {
+            return title;
+        }
+    }
 }
